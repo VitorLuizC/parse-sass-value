7afe489c5bb87ff86b8f7d7abcc2d7f821a47a42
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-04-16 17:14:39 -0300

    Create type definitions

---
+++ dist/parse-sass-value.d.ts
@@ -0,0 +1,12 @@
+declare type QuoteOption = 'none' | 'single' | 'double'
+
+declare type SeparatorOption = 'comma' | 'space'
+
+declare type ParseOptions = {
+  quote: QuoteOption,
+  separator: SeparatorOption
+}
+
+declare const parse: (value: any, options?: ParseOptions) => string;
+
+export { parse as default, ParseOptions }
