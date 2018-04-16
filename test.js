4f807b16e0fcd996c6c01ae0b6c3f96d72ab41c0
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-04-16 17:30:12 -0300

    Replace mocha + chain by ava

--- test.js
+++ test.js
@@ -1,74 +1,67 @@
-/* eslint-env mocha */
+import test from 'ava'
+import parse from './'
 
-const { assert } = require('chai')
+test('module exports a function', (context) => {
+  context.is(typeof parse, 'function')
+})
 
-describe('parse-sass-value module test', () => {
-  const parse = require('./')
+test('Throws error trying to parse functions', (context) => {
+  context.throws(() => parse(() => undefined))
+})
 
-  it('exports a function', () => {
-    assert.typeOf(parse, 'function')
-  })
+test('Throws error trying to parse undefined values', (context) => {
+  context.throws(() => parse(undefined))
+})
 
-  describe('unsupported values', () => {
-    it('Throws error trying to parse functions', () => {
-      assert.throws(() => parse(() => undefined))
-    })
-    it('Throws error trying to parse undefined values', () => {
-      assert.throws(() => parse(undefined))
-    })
-    it('Throws error trying to parse symbols', () => {
-      assert.throws(() => parse(Symbol('Example')))
-    })
-  })
+test('Throws error trying to parse symbols', (context) => {
+  context.throws(() => parse(Symbol('Example')))
+})
 
-  it('parses boolean values', () => {
-    assert.equal(parse(true), 'true')
-    assert.equal(parse(false), 'false')
-  })
+test('parses boolean values', (context) => {
+  context.is(parse(true), 'true')
+  context.is(parse(false), 'false')
+})
 
-  it('parses number values', () => {
-    assert.equal(parse(16), '16')
-    assert.equal(parse(0.99), '0.99')
-  })
+test('parses number values', (context) => {
+  context.is(parse(16), '16')
+  context.is(parse(0.99), '0.99')
+})
 
-  it('parses string values', () => {
-    assert.equal(parse('color-theme'), '\'color-theme\'')
-  })
+test('parses string values', (context) => {
+  context.is(parse('color-theme'), '\'color-theme\'')
+})
 
-  it('escape quotes', () => {
-    assert.equal(parse('"Open Sans"'), '\'\\"Open Sans\\"\'')
-    assert.equal(parse('\'Open Sans\''), '\'\\\'Open Sans\\\'\'')
-  })
+test('escape quotes', (context) => {
+  context.is(parse('"Open Sans"'), '\'\\"Open Sans\\"\'')
+  context.is(parse('\'Open Sans\''), '\'\\\'Open Sans\\\'\'')
+})
 
-  it('parses unit values without quotes', () => {
-    assert.equal(parse('100px'), '100px')
-    assert.equal(parse('10%'), '10%')
-    assert.equal(parse('12rem'), '12rem')
-  })
+test('parses unit values without quotes', (context) => {
+  context.is(parse('100px'), '100px')
+  context.is(parse('10%'), '10%')
+  context.is(parse('12rem'), '12rem')
+})
 
-  it('parses colors values without quotes', () => {
-    assert.equal(parse('white'), 'white')
-    assert.equal(parse('#333'), '#333')
-    assert.equal(parse('rgb(0,0,0)'), 'rgb(0,0,0)')
-  })
+test('parses colors values without quotes', (context) => {
+  context.is(parse('white'), 'white')
+  context.is(parse('#333'), '#333')
+  context.is(parse('rgb(0,0,0)'), 'rgb(0,0,0)')
+})
 
-  it('parses arrays to lists', () => {
-    assert.equal(parse([1, 2]), '(1, 2)')
-  })
+test('parses arrays to lists', (context) => {
+  context.is(parse([1, 2]), '(1, 2)')
+})
 
-  it('parses objects to maps', () => {
-    assert.equal(parse({a: 1, b: 2}), '(\'a\': 1, \'b\': 2)')
-  })
+test('parses objects to maps', (context) => {
+  context.is(parse({a: 1, b: 2}), '(\'a\': 1, \'b\': 2)')
+})
 
-  describe('options', () => {
-    it('quotes define string quote type', () => {
-      assert.equal(parse('color-theme', { quote: 'double' }), '"color-theme"')
-      assert.equal(parse('color-theme', { quote: 'single' }), '\'color-theme\'')
-    })
+test('options quotes define string quote type', (context) => {
+  context.is(parse('color-theme', { quote: 'double' }), '"color-theme"')
+  context.is(parse('color-theme', { quote: 'single' }), '\'color-theme\'')
+})
 
-    it('separator define list and map separator', () => {
-      assert.equal(parse(['10px', '20px'], { separator: 'comma' }), '(10px, 20px)')
-      assert.equal(parse(['10px', '20px'], { separator: 'space' }), '(10px 20px)')
-    })
-  })
+test('options separator define list and map separator', (context) => {
+  context.is(parse(['10px', '20px'], { separator: 'comma' }), '(10px, 20px)')
+  context.is(parse(['10px', '20px'], { separator: 'space' }), '(10px 20px)')
 })
